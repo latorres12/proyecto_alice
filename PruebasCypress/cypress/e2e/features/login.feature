1eba1a29198235776fdb2ea805a9006ab859b2fa
Luis Alfredo Torres Arenas <latorres@icesi.edu.co>
2023-05-08 18:04:44 -0500

    Primeras líneas de codificación

---
+++ PruebasCypress/cypress/e2e/features/login.feature
@@ -0,0 +1,22 @@
+Feature: Pruebas ALICE
+    
+    Feature se realizaran las pruebas correspondientes al app de almuerzos - ALICE
+
+    Scenario: Deberá cargar la página de login correctamente --> CUF-01
+        Given El usuario visitará la pagina
+        Then Verifica que la URL sea la correcta
+
+    Scenario: Datos de usuario válidos --> CUF-01
+        Given El usuario visitará la página
+        When Ingresará los datos de usuario y contraseña
+        Then Se carga la interfaz de usuario 
+
+    Scenario: Deberá mostrarle un mensaje de error al no ingresar usuario --> CUF-01
+        Given El usuario visitará la página
+        When No escribirá nombre de usuario e intentará loguear        
+        Then Verifica que arroje mensaje de usuario inválido
+
+    Scenario: Deberá mostrarle un mensaje de error al no ingresar clave --> CUF-01
+        Given El usuario visitará la página
+        When No escribirá la clave e intentará loguear        
+        Then Verifica que arroje mensaje de no clave
---
+++ PruebasCypress/cypress/e2e/step_definitions/login.js
@@ -0,0 +1,44 @@
+import {
+    Given,
+    When,
+    Then,  
+  } from "@badeball/cypress-cucumber-preprocessor";
+
+  Given("El usuario visitará la página", () => {
+    cy.visit("/");
+  });
+  
+//Validación de la URL de ALICE --> CUF-01
+  Then("Verifica que la URL sea la correcta", () => {
+    cy.url().should("eq", "https://192.168.218.243:8444/ic_apps/lunch/login");  
+  });
+  
+//Datos de Usuario válidos --> CUF-01
+  When("Ingresará los datos de usuario y contraseña", () => {
+    cy.get("#username").type("1110284954");
+    cy.get("#password").type("brdqlemBt7");
+    cy.get(".MuiButtonBase-root").click();
+  })
+  Then("Se carga la interfaz de usuario", () => {
+    cy.log("Todo bien");
+  })
+
+  When("No escribirá nombre de usuario e intentará loguear", () => {
+    cy.visit("/");
+    cy.get("#password").type("12345678");
+    cy.get('.MuiButtonBase-root.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary.MuiButton-sizeMedium.MuiButton-containedSizeMedium.MuiButton-fullWidth.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary.MuiButton-sizeMedium.MuiButton-containedSizeMedium.MuiButton-fullWidth.css-uvkclt').click();
+  });
+  
+  Then("Verifica que arroje mensaje de usuario inválido", () => {
+    cy.get('.swal2-html-container').should('have.text', 'El usuario no puede estar vacio');
+  });
+  
+  When("No escribirá la clave e intentará loguear", () => {
+    cy.visit("/");
+    cy.get("#username").type("94543140");
+    cy.get('.MuiButtonBase-root.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary.MuiButton-sizeMedium.MuiButton-containedSizeMedium.MuiButton-fullWidth.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary.MuiButton-sizeMedium.MuiButton-containedSizeMedium.MuiButton-fullWidth.css-uvkclt').click();
+  });
+  
+  Then("Verifica que arroje mensaje de no clave", () => {
+    cy.get('.swal2-html-container').should('have.text', 'La contraseña no puede estar vacia');
+  });
